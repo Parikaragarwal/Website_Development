bb4683f21151ba4484c5df1cf909bf5faa60b876
Parikaragarwal <parikaragarwal@gmail.com>
2025-02-23 18:50:48 +0530

    Notices from backend

--- server.js
+++ server.js
@@ -9,8 +9,12 @@ import awardRoutes from "./routes/awards.js"
 import { authMiddleware } from "./middlewares/auth.js";
 import Research from "./models/research.js";
 import Event from "./models/Events.js";
+import SNotice from "./models/studentnotice.js";
 import Award from "./models/Award.js"
 import eventsRouter from "./routes/Events.js";
+import FacultyNotice from "./models/facultynotice.js";
+import studentnoticeRouter from "./routes/studentnotice.js";
+import facultynoticeRouter from "./routes/facultynotice.js"
 
 dotenv.config(); // Load environment variables
 
@@ -38,14 +42,18 @@ app.use("/admin", adminRoutes);
 app.use("/admin/research", authMiddleware, researchRoutes);
 app.use("/admin/Events", authMiddleware ,eventsRouter);
 app.use("/admin/awards", authMiddleware ,awardRoutes);
+app.use("/admin/studentnotice",authMiddleware,studentnoticeRouter);
+app.use("/admin/facultynotice",authMiddleware,facultynoticeRouter);
 
 // Home Page Route
 app.get("/", async (req, res) => {
     try {
+        const studentnotices = await SNotice.find({});
+        const facultynotices = await FacultyNotice.find({});
         const events= await Event.find({});
         const awards = await Award.find({});
         const researchItems = await Research.find({}); // Fetch Research data
-        res.render("index", { researchItems,events,awards }); // Pass data to index.ejs
+        res.render("index", { researchItems,events,awards,studentnotices,facultynotices }); // Pass data to index.ejs
     } catch (err) {
         console.error(err);
         res.status(500).send("Server Error");
